151fef126d5afe46d5f10bf5f0da404a1ffa7fcc
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-10-15 17:20:04 +0100

    Added mutiple input component

---
+++ src/app/components/multiple-input/multiple-input.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MultipleInputComponent } from './multiple-input.component';
+
+describe('MultipleInputComponent', () => {
+  let component: MultipleInputComponent;
+  let fixture: ComponentFixture<MultipleInputComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MultipleInputComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MultipleInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
